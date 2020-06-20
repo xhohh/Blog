7090923c2b841f5e85ab7570801f6cc599d0097b
罗晨 <77878690@qq.com>
2020-06-20 12:31:37 +0800

    修改引用bug

--- src/FatTiger.Blog.Domain/FatTigerBlogDomainModule.cs
+++ src/FatTiger.Blog.Domain/FatTigerBlogDomainModule.cs
@@ -1,9 +1,13 @@
-﻿using Volo.Abp.Identity;
+﻿using FatTiger.Blog.Domain.Shared;
+using Volo.Abp.Identity;
 using Volo.Abp.Modularity;
 
 namespace FatTiger.Blog.Domain
 {
-    [DependsOn(typeof(AbpIdentityDomainModule))]
+    [DependsOn(
+        typeof(AbpIdentityDomainModule),
+        typeof(FatTigerBlogDomainSharedModule)
+        )]
     public class FatTigerBlogDomainModule : AbpModule
     {
 
